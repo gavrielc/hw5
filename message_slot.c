39f257f8e2780fb31f4a68fd7e0be95229e361ad
Gavriel Cohen <gavrielc@Gavrielc-mbp15.local>
2017-07-13 18:41:02 +0300

    6

--- message_slot.c
+++ message_slot.c
@@ -34,35 +34,35 @@ static node_t* head = NULL;
 
 static int device_open(struct inode *inode, struct file *file) {
     int id;
-    node_t* current;
+    node_t* current_node;
 
 
     printk("device_open(%p)\n", file);
 
 
     id = file->f_inode->i_ino;
-    current = head;
+    current_node = head;
 
-    while (current != NULL) {
-        if (current->id == id) {
-            current->open = 1;
+    while (current_node != NULL) {
+        if (current_node->id == id) {
+            current_node->open = 1;
             break;
         } else {
-            current = current->next;
+            current_node = current_node->next;
         }
     }
 
-    if (current == NULL) {
+    if (current_node == NULL) {
         if (head == NULL) {
             head = kmalloc(sizeof(struct node), GFP_KERNEL);
             head->id = id;
             head->open = 1;
         } else {
-            current = head;
-            while (current->next != NULL) current = current->next;
-            current->next = kmalloc(sizeof(struct node), GFP_KERNEL);
-            current->next->id = id;
-            current->next->open = 1;
+            current_node = head;
+            while (current_node->next != NULL) current_node = current_node->next;
+            current_node->next = kmalloc(sizeof(struct node), GFP_KERNEL);
+            current_node->next->id = id;
+            current_node->next->open = 1;
         }
     }
 
@@ -74,14 +74,14 @@ static int device_release(struct inode *inode, struct file *file) {
 
     int id = file->f_inode->i_ino;
 
-    node_t* current = head;
+    node_t* current_node = head;
 
-    while (current != NULL) {
-        if (current->id == id) {
-            current->open = 0;
+    while (current_node != NULL) {
+        if (current_node->id == id) {
+            current_node->open = 0;
             break;
         } else {
-            current = current->next;
+            current_node = current_node->next;
         }
     }
 
@@ -103,26 +103,26 @@ static ssize_t device_write(struct file *file, const char __user * buffer, size_
 
     int id = file->f_inode->i_ino;
 
-    node_t* current = head;
+    node_t* current_node = head;
 
-    while (current != NULL && current->id != id) current = current->next;
+    while (current_node != NULL && current_node->id != id) current_node = current_node->next;
 
-    if (current == NULL) {
+    if (current_node == NULL) {
         //  TODO file doesnt exist
-    } else if (!(current->open)) {
+    } else if (!(current_node->open)) {
         //  TODO closed
     }
 
-    int index = current->data.index;
+    int index = current_node->data.index;
     //TODO check index
 
     for (i = 0; i < 128; i++) {
         if (i < length) {
-            if (-EFAULT == get_user(current->data.buffers[index][i], buffer + i)) {
+            if (-EFAULT == get_user(current_node->data.buffers[index][i], buffer + i)) {
                 //TODO handle error
             }
         } else {
-            current->data.buffers[index][i] = '0';
+            current_node->data.buffers[index][i] = '0';
         }
     }
 
@@ -134,11 +134,11 @@ static long device_ioctl(struct file* file, unsigned int ioctl_num, unsigned lon
 
   if (IOCTL_SET_ENC == ioctl_num && ioctl_param > -1 && ioctl_param < 4) {
     printk("chardev, ioctl: setting index to %ld\n", ioctl_param);
-    node_t* current = head;
+    node_t* current_node = head;
     int id = file->f_inode->i_ino;
-    while (current->id != id) current = current->next;
-    if (current->open) {
-        current->data.index = ioctl_param;
+    while (current_node->id != id) current_node = current_node->next;
+    if (current_node->open) {
+        current_node->data.index = ioctl_param;
     } else {
         //TODO ERORR
     }
@@ -181,13 +181,13 @@ static int __init simple_init(void) {
 
 
 static void __exit simple_cleanup(void) {
-    node_t* current head;
+    node_t* current_node head;
     node_t* next;
 
-    while (current != NULL) {
-        next = current->next;
-        free(current);
-        current = next;
+    while (current_node != NULL) {
+        next = current_node->next;
+        free(current_node);
+        current_node = next;
     }
 
     unregister_chrdev(MAJOR_NUM, DEVICE_RANGE_NAME);
