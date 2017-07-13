b9515c3782ba21ba8a137a8709386c9e322b050d
Gavriel Cohen <gavrielc@Gavrielc-mbp15.local>
2017-07-13 19:09:30 +0300

    9

--- message_slot.h
+++ message_slot.h
@@ -1,22 +1,17 @@
-#ifndef CHARDEV_H
-#define CHARDEV_H
+#ifndef MESSAGE_SLOT_H
+#define MESSAGE_SLOT_H
 
 #include <linux/ioctl.h>
 
 
-
-/* The major device number. We can't rely on dynamic 
- * registration any more, because ioctls need to know 
- * it. */
 #define MAJOR_NUM 245
 
 
-/* Set the message of the device driver */
 #define IOCTL_SET_ENC _IOW(MAJOR_NUM, 0, unsigned long)
 
 #define DEVICE_RANGE_NAME "char_dev"
 #define BUF_LEN 80
-#define DEVICE_FILE_NAME "simple_char_dev"
+#define DEVICE_FILE_NAME "message_slot"
 #define SUCCESS 0
 
 
